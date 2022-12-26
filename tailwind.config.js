1cf7e8964af5a6d0b464b2fbfc878f8b194b3244
sufian <sufian4se@gmail.com>
2022-12-26 19:55:16 +0600

    initialaization

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    colors: {
+      primary: '#f09022',
+      secondary: '#07b81f',
+    },
+    extend: {},
+  },
+  plugins: [],
+}
